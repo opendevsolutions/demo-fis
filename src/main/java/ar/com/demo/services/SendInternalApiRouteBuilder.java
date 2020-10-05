f8a0ce8a3e2bfbbad8220f4a0617ae623f7c020a
jamadrigale <jm@opendevsolutions.com.ar>
2020-10-05 19:40:33 -0300

    change vars

--- src/main/java/ar/com/demo/services/SendInternalApiRouteBuilder.java
+++ src/main/java/ar/com/demo/services/SendInternalApiRouteBuilder.java
@@ -1,7 +1,6 @@
 package ar.com.demo.services;
 
 import org.apache.camel.Exchange;
-import org.apache.camel.component.jackson.JacksonDataFormat;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,9 +13,8 @@ public class SendInternalApiRouteBuilder extends BaseRouteBuilder {
 	  	 .to("direct:callSSO")
 	     .setHeader(Exchange.HTTP_METHOD, constant("GET"))
 	     .setHeader("Content-Type", constant("application/json"))
-		 .to("http4://{{internal.servece.host}}/?bridgeEndpoint=true&amp;throwExceptionOnFailure=false")
-		 .unmarshal(new JacksonDataFormat(Object.class))
-		 .setHeader("Content-Type", constant("application/json"));
+	     .setBody().simple("")
+		 .to("http4://{{internal.servece.host}}/?bridgeEndpoint=true&amp;throwExceptionOnFailure=false");
   }
   
 
