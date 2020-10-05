472538446280eedbe820bbac5410400e62b5eb45
jamadrigale <jm@opendevsolutions.com.ar>
2020-10-05 02:49:29 -0300

    some fix

--- src/main/java/ar/com/demo/services/SendInternalApiRouteBuilder.java
+++ src/main/java/ar/com/demo/services/SendInternalApiRouteBuilder.java
@@ -1,6 +1,7 @@
 package ar.com.demo.services;
 
 import org.apache.camel.Exchange;
+import org.apache.camel.component.jackson.JacksonDataFormat;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,6 +15,7 @@ public class SendInternalApiRouteBuilder extends BaseRouteBuilder {
 	     .setHeader(Exchange.HTTP_METHOD, constant("GET"))
 	     .setHeader("Content-Type", constant("application/json"))
 		 .to("http4://{{internal.servece.host}}/?bridgeEndpoint=true&amp;throwExceptionOnFailure=false")
+		 .unmarshal(new JacksonDataFormat(Object.class))
 		 .setHeader("Content-Type", constant("application/json"));
   }
   
